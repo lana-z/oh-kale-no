e086088835e8b346c1a6dfb8c89a744d2f066502
lana-z <lana@levelupeconomy.com>
2024-11-15 14:19:03 -0800

    revert vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/core': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-        credentials: 'include',
-      }
-    }
 })
