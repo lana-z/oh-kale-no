4ec8e09149e5a6d29fa46f3a60a4f2fb61fa3a00
lana-z <lana@levelupeconomy.com>
2024-11-18 18:38:47 -0800

    refactor for redundant token

--- src/api_calls/CsrfToken.js
+++ src/api_calls/CsrfToken.js
@@ -4,45 +4,58 @@ const VITE_API_BASE_URL = import.meta.env.PROD
 
 let csrfToken = null;
 
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
 export async function fetchCsrfToken() {
-    if (csrfToken) {
-        return csrfToken;
-    }
-    try {
-        console.log('Fetching CSRF token...');
-        const response = await fetch(`${VITE_API_BASE_URL}/core/get-csrf-token/`, {
-            method: 'GET',
-            credentials: 'include',
-            headers: {
-                'Accept': 'application/json',
-                'X-Requested-With': 'XMLHttpRequest',
-            }
-        });
-        
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
-            const [name, value] = cookie.split('=');
-            acc[name] = value;
-            return acc;
-        }, {});
-
-        csrfToken = cookies['csrftoken'] || null
-
-        console.log('csrfToken returned from fetchCsrfToken:', csrfToken);
-        console.log('document.cookie in fetchCsrfToken: ', document.cookie);
-        
+    if (!csrfToken) {
+        csrfToken = getCookie('csrftoken');
         if (!csrfToken) {
             throw new Error('CSRF token not found in cookies.');
         }
-        
-        return csrfToken; 
-    } catch (error) {
-        console.error('Error fetching CSRF token: ', error);
-        throw error;
     }
+    return csrfToken;
 }
 
 export default fetchCsrfToken;
+
+
+// export async function fetchCsrfToken() {
+//     if (csrfToken) {
+//         return csrfToken;
+//     }
+//     try {
+//         console.log('Fetching CSRF token...');
+//         const response = await fetch(`${VITE_API_BASE_URL}/core/get-csrf-token/`, {
+//             method: 'GET',
+//             credentials: 'include',
+//             headers: {
+//                 'Accept': 'application/json',
+//                 'X-Requested-With': 'XMLHttpRequest',
+//             }
+//         });
+        
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! Status: ${response.status}`);
+//         }
+
+//         const data = await response.json();
+//         csrfToken = data.csrfToken;
+
+//         console.log('csrfToken returned from fetchCsrfToken:', csrfToken);
+        
+//         if (!csrfToken) {
+//             throw new Error('CSRF token not found in response.');
+//         }
+        
+//         return csrfToken; 
+//     } catch (error) {
+//         console.error('Error fetching CSRF token: ', error);
+//         throw error;
+//     }
+// }
+
+// export default fetchCsrfToken;
